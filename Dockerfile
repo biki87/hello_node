b29d663e6cdb9107ba2f104b311634f8c3027d9f
Dima Bikov <bikov.dima@gmail.com>
2023-06-17 18:04:02 +0300

    change port to 80

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM node:16-alpine
 WORKDIR /user/src/app
 COPY hello.js .
-EXPOSE 443
+EXPOSE 80
 CMD ["node", "hello.js"]
--- hello.js
+++ hello.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Hello World\n');
-}).listen(443);
+}).listen(80);
 
-console.log('Server running at http://127.0.0.1:443/');
+console.log('Server running at http://127.0.0.1:80/');
